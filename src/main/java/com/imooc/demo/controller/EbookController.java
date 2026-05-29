3bc8ec02df76a8a843d8216a66fc4aea1bab7605
user <1269936630@qq.com>
2026-05-29 09:01:31 +0800

    1.4 封装请求参数和返回参数

--- src/main/java/com/imooc/demo/controller/EbookController.java
+++ src/main/java/com/imooc/demo/controller/EbookController.java
@@ -1,6 +1,8 @@
 package com.imooc.demo.controller;
 import com.imooc.demo.domain.Ebook;
+import com.imooc.demo.req.EbookReq;
 import com.imooc.demo.resp.CommonResp;
+import com.imooc.demo.resp.EbookResp;
 import com.imooc.demo.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,24 +16,21 @@ public class EbookController {
 
     //@Value("${ebook.hello}")
     //private String ebookHello;
-
     @Autowired
     private EbookService ebookService;
-
     //@RequestMapping(value = "/hello",method = RequestMethod.GET)
     //public String hello(){
     //    return "hello world!!!" + ebookHello;
     //}
-
     //@RequestMapping(value = "/hello/post",method = RequestMethod.POST)
     //public String helloPost(String name){
     //    return "hello world!!!" + name;
     //}
 
     @GetMapping("/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/imooc/demo/service/EbookService.java
+++ src/main/java/com/imooc/demo/service/EbookService.java
@@ -1,8 +1,14 @@
 package com.imooc.demo.service;
 import com.imooc.demo.domain.Ebook;
+import com.imooc.demo.domain.EbookExample;
 import com.imooc.demo.mapper.EbookMapper;
+import com.imooc.demo.req.EbookReq;
+import com.imooc.demo.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -11,7 +17,17 @@ public class EbookService {
     @Autowired
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%"+req.getName()+"%");
+        List<Ebook> ebooksList = ebookMapper.selectByExample(ebookExample);
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebooksList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 }
