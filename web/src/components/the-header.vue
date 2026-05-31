8975d4a83dc88c5a29af033efbdbaf15426d99bd
user <1269936630@qq.com>
2026-05-31 10:24:53 +0800

    2.1 制作the-header组件

---
+++ web/src/components/the-header.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-layout-header class="header">
+
+
+  </a-layout-header>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-header',
+  props: {
+    msg: String,
+  },
+});
+</script>
