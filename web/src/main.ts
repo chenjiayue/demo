87e33d75f1f8eb1132aaa49d53b91ec5e49730d4
user <1269936630@qq.com>
2026-06-01 21:50:20 +0800

    2.4 继承图标组件 npm install @ant-design/icons-vue --save

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,5 +4,11 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue'
 //import 'ant-design-vue/dist'
+import * as Icons from '@ant-design/icons-vue';
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
+const icons: any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
 
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+}
