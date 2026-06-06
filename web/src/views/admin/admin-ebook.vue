9dfe6eaed8e7369b96588b9d65e6362aafa2568d
user <1269936630@qq.com>
2026-06-06 17:13:47 +0800

    4.6 电子书管理页面前后端分页整合完成

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,3 +1,4 @@
+<!--
 <template>
   <a-layout-footer style="text-align: center">
     页面管理
@@ -12,4 +13,152 @@ export default defineComponent({
     msg: String,
   },
 });
+</script>-->
+<template>
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <p>
+        <a-form layout="inline" :model="param">
+          <a-form-item>
+            <a-input v-model:value="param.name" placeholder="名称">
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="handleQuery({page: 2, size: pagination.pageSize})">
+              查询
+            </a-button>
+          </a-form-item>
+
+        </a-form>
+      </p>
+    </a-layout-content>
+  </a-layout>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import axios from 'axios';
+import { message } from 'ant-design-vue';
+import {Tool} from "@/util/tool";
+
+export default defineComponent({
+  name: 'AdminEbook',
+  setup() {
+    const param = ref();
+    param.value = {};
+    const ebooks = ref();
+    const pagination = ref({
+      current: 1,
+      pageSize: 3,
+      total: 0
+    });
+    const loading = ref(false);
+
+    const columns = [
+      {
+        title: '封面',
+        dataIndex: 'cover',
+        slots: { customRender: 'cover' }
+      },
+      {
+        title: '名称',
+        dataIndex: 'name'
+      },
+      {
+        title: '分类',
+        slots: { customRender: 'category' }
+      },
+      {
+        title: '文档数',
+        dataIndex: 'docCount'
+      },
+      {
+        title: '阅读数',
+        dataIndex: 'viewCount'
+      },
+      {
+        title: '点赞数',
+        dataIndex: 'voteCount'
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        slots: { customRender: 'action' }
+      }
+    ];
+
+    /**
+     * 数据查询
+     **/
+    const handleQuery = (params: any) => {
+      loading.value = true;
+      // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+      ebooks.value = [];
+      axios.get("/ebook/list", {
+        params: {
+          page: params.page,
+          size: params.size,
+          name: param.value.name
+        }
+      }).then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if (data.success) {
+          ebooks.value = data.content.list;
+
+          // 重置分页按钮
+          pagination.value.current = params.page;
+          pagination.value.total = data.content.total;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    /**
+     * 表格点击页码时触发
+     */
+    const handleTableChange = (pagination: any) => {
+      console.log("看看自带的分页参数都有啥：" + pagination);
+      handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      });
+    };
+
+    // -------- 表单 ---------
+    /**
+     * 数组，[100, 101]对应：前端开发 / Vue
+     */
+
+    onMounted(() => {
+      handleQuery({
+        page: 1,
+        size: pagination.value.pageSize ,
+      });
+    });
+
+    return {
+      param,
+      ebooks,
+      pagination,
+      columns,
+      loading,
+      handleTableChange,
+      handleQuery,
+
+    }
+  }
+});
 </script>
+
+<style scoped>
+img {
+  width: 50px;
+  height: 50px;
+}
+</style>
+
