c078fafebedd68e7920b7cf2ae64f31d3982ef82
user <1269936630@qq.com>
2026-07-16 16:57:55 +0800

    5.1.1增加logback配置文件

--- src/main/java/com/imooc/demo/config/Application.java
+++ src/main/java/com/imooc/demo/config/Application.java
@@ -1,9 +1,10 @@
-package com.imooc.demo.config;
+/*package com.imooc.demo.config;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;*/
+/*
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
@@ -21,6 +22,39 @@ public class Application {
         System.out.println("启动成功！!");
         System.out.println("根地址: \t\thttp://127.0.0.1:" + env.getProperty("server.port"));
         System.out.println("登录接口: \thttp://127.0.0.1:" + env.getProperty("server.port") + "/user/login?loginName=test&password=test");
+    }
+
+}*/
+package com.imooc.demo.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.imooc.demo")
+@MapperScan("com.imooc.demo.mapper")
+public class Application {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        Environment env = context.getEnvironment();
+        String port = env.getProperty("server.port", "8080");
+
+        LOG.info("启动成功！！");
+        LOG.info("根地址: \t\thttp://127.0.0.1:{}", port);
+        LOG.info("登录接口: \thttp://127.0.0.1:{}/user/login?loginName=test&password=test", port);
+
+        System.out.println("启动成功！！");
+        System.out.println("根地址: \t\thttp://127.0.0.1:" + port);
+        System.out.println("登录接口: \thttp://127.0.0.1:" + port + "/user/login?loginName=test&password=test");
     }
 
 }
