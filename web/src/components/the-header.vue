9d0a4ddae91f1076b7c71d74dee1a992bc10dccb
user <1269936630@qq.com>
2026-07-18 15:39:33 +0800

    5.1.5 集成图标组件 npm install @ant-design/icons-vue --save

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -7,9 +7,9 @@
         v-model:selectedKeys="selectedKeys1"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 1</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
+      <a-menu-item key="1" style="left: 150px">nav 1</a-menu-item>
+      <a-menu-item key="2" style="left: 150px">nav 2</a-menu-item>
+      <a-menu-item key="3" style="left: 150px">nav 3</a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
